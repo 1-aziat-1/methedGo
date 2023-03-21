e10a5822b9be8fe768ed950195129cacb764ce8b
1-aziat-1 <as.ac2727@mail.ru>
2023-03-21 22:54:24 +0300

    ready menu and modal contact

--- script/module/modal.js
+++ script/module/modal.js
@@ -1,23 +1,60 @@
 const headerBuy = document.querySelector('.header__buy');
-const modal = document.querySelector('.modal');
+const formOverlay = document.querySelector('.form__overlay');
+const menuBtn = document.querySelector('.menu');
+const menu = document.querySelector('.dropdown__menu');
+const modalBtn = menu.querySelector('.modal__btn');
+const menuItems = menu.querySelectorAll('.dropdown__menu-list>li')
 
+const menuControl = () => {
+  const menuOpen = () => {
+    menu.classList.add('is-visible');
+    menuBtn.classList.add('is-active');
+  };
+
+  const menuClose = () => {
+    menu.classList.remove('is-visible');
+    menuBtn.classList.remove('is-active');
+  };
+
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.contains('is-active') ?
+      menuClose() : menuOpen();
+  });
+
+  menu.addEventListener('click', ({target}) => {
+    if (target === menu || target.closest('li') ) {
+      menuClose();
+    }
+  });
+  
+};
 
 const modalControl = () => {
   const modalOpen = () => {
-    modal.classList.add('is-visible');
+    formOverlay.classList.add('is-visible');
+    if (menuBtn.classList.contains('is-active')) {
+      menu.classList.remove('is-visible');
+      menuBtn.classList.remove('is-active');
+    }
   };
 
   const modalClose = () => {
-    modal.classList.remove('is-visible');
+    formOverlay.classList.remove('is-visible');
   };
 
   headerBuy.addEventListener('click', modalOpen);
+  modalBtn.addEventListener('click', () => {
+    modalOpen();
+  });
 
-  modal.addEventListener('click', ({target}) => {
-    if (target.classList.contains('close') || target === modal) {
+  formOverlay.addEventListener('click', ({target}) => {
+    if (target.classList.contains('close') || target === formOverlay) {
       modalClose();
     }
   });
 };
 
+
+
 modalControl();
+menuControl();
